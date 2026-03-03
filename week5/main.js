e403ebc3052b7aaa08f98c819486249ffc2f47b5
Tanzeela Jahangir <tj2657@nyu.edu>
2026-03-03 13:42:00 -0500

    Add files via upload

---
+++ week5/main.js
@@ -0,0 +1,17 @@
+// Week 5 Project - JavaScript
+
+console.log('🚀 Week 5 Project loaded!');
+
+// Get elements
+const myButton = document.getElementById('myButton');
+
+// Button click handler
+if (myButton) {
+    myButton.addEventListener('click', () => {
+        console.log('Button clicked!');
+        alert('🎉 Hello from Week 5! You can now build awesome things with HTML, CSS, and JavaScript!');
+    });
+}
+
+// Add more functionality here
+console.log('Ready to code!');
